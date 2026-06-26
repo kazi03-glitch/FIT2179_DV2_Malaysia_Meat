8798a6f7337dbccc0723ce8d02245bb7f9a01cf1
kazi03-glitch <kaziahnafislam@gmail.com>
2026-06-26 19:00:54 +0800

    Add files via upload
    Initial project upload

---
+++ js/embed.js
@@ -0,0 +1,47 @@
+vegaEmbed("#global_context_map", "specs/01_global_context_map.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#risk_profile_heatmap", "specs/13_supply_risk_profile.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#latest_lollipop", "specs/05_latest_lollipop.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#consumption_trend", "specs/03_consumption_trend_brush.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#meat_share_area", "specs/04_meat_share_area.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#ssr_bullet", "specs/06_ssr_bullet.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#ssr_slope", "specs/07_ssr_slope.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#prod_vs_cons", "specs/08_prod_vs_cons.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#trade_balance", "specs/09_trade_balance.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#import_heatmap", "specs/10_import_heatmap.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#security_quadrant", "specs/11_food_security_quadrant.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#meat_basket", "specs/12_meat_basket_waffle.vg.json", {
+  actions: false
+}).catch(console.error);
